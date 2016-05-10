b697d3006267a2c6b1781141ac2be53b2d33b3f5
muyuqingqiu <liuz5547815@aliyun.com>
2016-05-10 15:42:19 +0800

    all

--- js/geoM.js
+++ js/geoM.js
@@ -1,11 +1,11 @@
 define(function(require,exports,module){
-	//读取和设定地理信息的模块	
+	//读取和设定地理信息的模块
 	var cookie = require('cookie');
 	function setGeo(obj){ //传入的需要是jq对象
 		if(cookie.getCookie('geo_ad')){
 			obj.html(cookie.getCookie('geo_ad'));
-		}else{				
-			plus.geolocation.getCurrentPosition( function (p) {	
+		}else{
+			plus.geolocation.getCurrentPosition( function (p) {
 				var city = p.address.city;
 				obj.html(city);
 				cookie.setCookie('geo_ad',city,10);	
@@ -13,9 +13,7 @@ define(function(require,exports,module){
 				alert('地理信息获取失败，请重新获取');
 			} );
 		}
-		
+
 	}
 	exports.setGeo = setGeo;
-})
-
-
+});
